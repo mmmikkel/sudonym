9467638ddde002a6a8fa29088e4a0dfa5070bbc6
mmmikkel <michael@duyvesteijn.net>
2017-12-27 14:03:11 +0100

    dsfafads
    asdffasdfdas

--- michael_spider.py
+++ michael_spider.py
@@ -1,14 +1,38 @@
 import scrapy
-
+import re
 
 class QuotesSpider(scrapy.Spider):
-    name = "michaelemail"
-    start_urls = [
-        'https://michael.duyvesteijn.net',
-    ]
+	name = "michaelemail"
+	start_urls = [
+		'https://michael.duyvesteijn.net/',
+	]
 
-    def parse(self, response):
-        for michaelemail in response.css('div.textwidget'):
-            yield {
-                'mail': michaelemail.css('a::attr("href")').extract_first(),
-            }
+	def parse(self, response):
+		privacyLink = response.xpath('//li/a[re:test(@href,".*how.*")]/@href').extract_first()
+		if privacyLink is not None:
+			yield response.follow(privacyLink, callback=self.parse1)
+
+	def parse1(self, response):
+
+ 		m = response.css('h1::text').re(r'.*')
+
+ 		#This below all works
+		#privacy = response.css('li a::text').re(r'How.*')
+		#privacy = response.css('li.page_item.page-item-17 a::attr(href)').extract_first()
+		#privacy2 = response.xpath('//li/a/text()').extract_first()
+		#privacy2 = response.xpath('//li/a/text()').re(r'How.*')
+		#privacy2 = response.xpath('//li/a[re:test(@href,".*how.*")]/@href').extract_first()
+
+
+		#divs = response.xpath('//div')
+		#for a in divs.xpath('.//a[re:test(@href, "(<)?(\w+@\w+(?:\.\w+)+)(?(1)>|$)")]/@href'):  # extracts all <p> inside
+		#for a in divs.xpath('.//p[re:test(text(), ".*Operations.*")]/text()'):
+		divs = response.xpath('//div/p[re:test(text(), ".*Operations.*")]/text()')
+		#divs.xpath('.//a[re:test(@href, "(<)?(\w+@\w+(?:\.\w+)+)(?(1)>|$)")]/@href')  # extracts all <p> inside
+
+		yield {
+		    'href': divs.extract(),
+		    'm': m,
+		    #'privacy': privacy,
+		    #'privacy2':privacy2,
+		}
